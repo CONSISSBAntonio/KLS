b8ec9b2c9211498b26734270f0ef5584911d598a
Braulio Antonio Hernandez <braulio.antonio@consiss.com>
2021-07-02 10:08:29 -0500

    Correccion bugs

---
+++ KLS_WEB/KLS_WEB/Controllers/Clients/Requirements/RequirementsController.cs
@@ -0,0 +1,31 @@
+﻿using KLS_WEB.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KLS_WEB.Controllers.Clients.Requirements
+{
+    [Route("Clients/Requirements")]
+    [Authorize]
+    public class RequirementsController : Controller
+    {
+        private string _UrlView = "~/Views/Clients/Requirements/";
+        private string _UrlApi = "Clients/Requirements";
+        private readonly IAppContextService AppContext;
+
+        public RequirementsController(IAppContextService _AppContext)
+        {
+            this.AppContext = _AppContext;
+        }
+
+        [Route("{id=0}")]
+        public IActionResult Index(int id)
+        {
+            ViewBag.id = id;
+            return View(this._UrlView + "index.cshtml");
+        }
+    }
+}
