4a9ea22c3e381b02e110babbd8548338c1d2e884
Francisco Robles <francisco.robles@consiss.com>
2021-07-13 11:37:01 -0700

    * Nuevo viaje bugfixes

--- KLS_WEB/KLS_WEB/Models/Travels/TravelDT.cs
+++ KLS_WEB/KLS_WEB/Models/Travels/TravelDT.cs
@@ -15,8 +15,8 @@ namespace KLS_WEB.Models.Travels
         public string Destino { get; set; }
         public DateTime FechaSalida { get; set; }
         public DateTime FechaLlegada { get; set; }
-        public string Salida => FechaSalida.ToString("g");
-        public string Llegada => FechaLlegada.ToString("g");
+        public string Salida => FechaSalida.ToString("dd/MM/yyyy. HH:mm tt");
+        public string Llegada => FechaLlegada.ToString("dd/MM/yyyy. HH:mm tt");
         public string Estatus { get; set; }
     }
 }
