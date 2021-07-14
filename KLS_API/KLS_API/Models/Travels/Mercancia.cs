e18156ffbf844d97c44fc367c88ac64bea543ae2
Francisco Robles <francisco.robles@consiss.com>
2021-07-13 17:02:11 -0700

    + migracion mercancias

---
+++ KLS_API/KLS_API/Models/Travels/Mercancia.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KLS_API.Models.Travels
+{
+    public class Mercancia
+    {
+        public int Id { get; set; }
+        public int TravelId { get; set; }
+        public Travel Travel { get; set; }
+
+        [Column(TypeName = "decimal(18,4)")]
+        public decimal Alto { get; set; }
+        [Column(TypeName = "decimal(18,4)")]
+        public decimal Ancho { get; set; }
+        [Column(TypeName = "decimal(18,4)")]
+        public decimal Largo { get; set; }
+        [Column(TypeName = "decimal(18,4)")]
+        public decimal Peso { get; set; }
+        [Column(TypeName = "decimal(18,4)")]
+        public decimal PesoVolumetrico { get; set; }
+    }
+}
