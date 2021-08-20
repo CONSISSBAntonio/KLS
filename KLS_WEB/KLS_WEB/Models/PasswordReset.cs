2533458011d53ec91dfe695ae95afcdb2a777fac
CONSISSFRobles <francisco.robles@consiss.com>
2021-08-20 15:12:23 -0500

    +Password reset
    falta agregar la cuenta de correo smtp

---
+++ KLS_WEB/KLS_WEB/Models/PasswordReset.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KLS_WEB.Models
+{
+    public class PasswordReset
+    {
+
+        [Required(ErrorMessage = "La contraseña es obligatoria", AllowEmptyStrings = false)]
+        [DataType(DataType.Password)]
+        public string NewPassword { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("NewPassword", ErrorMessage = "Las contraseñas no coinciden")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        public string ResetToken { get; set; }
+        public bool IsValid { get; set; }
+    }
+}
